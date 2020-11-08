cc17ac1f6d2c1d8071d1167a94370f3f8e07be6b
Illusion of control <sergeyskorokhod2@gmail.com>
2020-11-09 01:02:49 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function ListItem(props) {
-  const key = props.key
-  const name = props.name;
-  const price = props.price
+  const item = props.item
   return(
-    <li key={key}>
-      <p>name: {name}</p>
-      <p>price: {price}</p>
+    <li id={item.id}>
+      <p>name: {item.name}</p>
+      <p>price: {item.price}</p>
     </li>
   )
 }
 
+
 function App(props) {
   const items = props.items
   const listItems = items.map((item) =>
-    <ListItem key={item.id.toString()} name={item.name} price={item.price} />
+    <ListItem key={item.id} item={item} />
   )
   return (
     <ul>
