35c2e618eac0f78782c21712b417b61d7496d6dd
Illusion_of_control <sergeyskorokhod2@gmail.com>
2022-07-17 19:46:18 +0300

    added imports of components

--- 06-menu/src/components/index.js
+++ 06-menu/src/components/index.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import Categories from "./Categories";
 import Menu from "./Menu";
+import CartItem from './CartItem'
 
-export { Navbar, Categories, Menu };
+export { Navbar, Categories, Menu, CartItem };
