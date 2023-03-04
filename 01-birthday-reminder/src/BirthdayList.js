03234f0071f44929834e6f739965ae8807c5f58e
Illusion of control <sergeyskorokhod2@gmail.com>
2023-03-04 17:31:14 +0300

    reformatted page structure

--- 01-birthday-reminder/src/BirthdayList.js
+++ 01-birthday-reminder/src/BirthdayList.js
@@ -3,20 +3,20 @@ import React from 'react'
 
 const BirthdayList = ({persons}) => {
   return(
-    <>
+    <ul>
       {persons.map((person) => {
         const {id, name, age, image_url} = person;
         return (
-          <article key={id} className='person'>
+          <li key={id} className='person'>
             <img src={image_url} alt={name} />
             <div>
               <h4>{name}</h4>
               <p>{age} years</p>
             </div>
-          </article>
+          </li>
         )
       })}
-    </>
+    </ul>
   )
 }
 
